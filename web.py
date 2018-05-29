811c099f34e27caca2d6907305688f72f42b99cd
bindingfly <2534270570@qq.com>
2018-05-29 22:41:05 +0800

    [web]try to open website

---
+++ web.py
@@ -0,0 +1,9 @@
+#! python
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else :
+    address = pyperclip.paste()
+
+webbrowser.open('https://map.baidu.com' + address)
